a1988a531c6b7dfe44a01e5417fea2a55b5cc91e
Battincik <battincik@gmail.com>
2024-12-26 20:55:25 +0300

    new logger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import { Elysia } from "elysia";
+import { logger } from "@tqman/nice-logger";
 
-const app = new Elysia().get("/", () => "Hello Battincik").listen(3000);
+const app = new Elysia();
+
+app.use(
+    logger({
+        mode: "combined",
+        withTimestamp: true,
+        withBanner: true,
+    })
+);
+
+app.get("/", () => "Hello Battincik");
+app.listen(3000);
 
 console.log(
     `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
